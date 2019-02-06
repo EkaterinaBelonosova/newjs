89f5d84480e8a312c673cbfbe57bdc64c74f6187
Ekaterina <katerina.belonosova@gmail.com>
2019-02-06 12:19:39 +0700

    Задание 1

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
-let arr =['plum.png','oran.jpg','apple.bmp'];
+//let arr =['plum.png','oran.jpg','apple.bmp'];
 
-console.log(arr[1]);
+//console.log(arr[1]);
 
 //alert("hello");
 
-let answer = prompt("Тут поль?", "Да");
-
-console.log(answer);
+let money = +prompt("Ваш бюджет на месяц?", "25");
+    console.log(money);
+let time = prompt("Введите дату в формате YYYY-MM-DD", "YYYY-MM-DD");
+    console.log(time);
+let vopr1 = prompt("Введите обязательную статью расходов в этом месяце", " ");
+    console.log(vopr1);
+let vopr2 = prompt("Во сколько обойдется?", " ");
+    console.log(vopr2);
+let arr={vopr1 : vopr2};
+let appData = {
+    cost:money,
+    timeData:time,
+    expenses:arr,
+    optionalExpenses:null,
+    income:[],
+    savings:false
+};
+    console.log(appData);
+    
+    alert("Бюджет на 1 день "+ money/30);
