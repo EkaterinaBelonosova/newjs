2939c18f6a5d403085c347cea26b514ad164371e
Ekaterina <katerina.belonosova@gmail.com>
2019-02-20 16:04:19 +0700

    promise and slider

--- Задание 7/js/script.js
+++ Задание 7/js/script.js
@@ -132,4 +132,196 @@ window.addEventListener('DOMContentLoaded', function() {    //запускать
     });
     }
     console.log(desbtn);
+
+
+    // Form without promise
+    /*let message = {
+        loading: 'Загрузка...',
+        success: 'Спасибо! Скоро мы с вами свяжемся!',
+        failure: 'Что-то пошло не так...'
+    };
+
+    let form = document.querySelector('.main-form'),
+        formBootom = document.getElementById('form'),
+        input = form.getElementsByTagName('input'),
+        statusMessage = document.createElement('div'); //создаем див для передачи пользователю сообщения статуса    
+        statusMessage.classList.add('status');
+
+    function sendForm(elem){
+        elem.addEventListener('submit', function(event) {//вещаем обработчик события именно на форму, а не на кнопку в форме, потому что нам нужно знать когда именно форма отправляется на сервер
+            event.preventDefault(); //для того чтобы не обновлялась страница при нажатии на кнопку форму(обновление браузера)
+                elem.appendChild(statusMessage);
+
+                let request = new XMLHttpRequest();
+                request.open('POST', 'server.php');
+                //request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded'); 
+                
+                //for json
+                request.setRequestHeader('Content-type', 'application/json; charset=uft-8');
+                //end for json
+                
+
+                let formData = new FormData(elem); //с помощью объекта формдата получаем все что пользовтаель ответил  в форме
+
+                //for json
+                let obj ={};  //создем новый объект в который помещаем данные
+
+                formData.forEach(function(value,key) { //с помощью метода forEach берем все данные из формдаты и помещаем в объект obj
+                        obj[key] = value;
+                    });
+                let json = JSON.stringify(obj); //превращаем в json формат
+                request.send(json); // отправляем на сервер
+                //end for json
+                //request.send(formData); //форм дата - это данные которые ввел пользователь 
+                request.onreadystatechange = function(){ //чтобы отлеживать статусы отправлений
+                    if(request.readyState<4){
+                        statusMessage.innerHTML = message.loading;
+                    }else if(request.readyState === 4){
+                        if(request.status == 200 && request.status < 300){
+                            statusMessage.innerHTML = message.success;
+                        }
+                        else{
+                            console.log(request.status);
+                            statusMessage.innerHTML = message.failure;
+                        }
+                    }
+                }
+
+                for (let i=0; i<input.length; i++){
+                    input[i].value='';
+                }  
+        }); 
+    }
+
+    sendForm(form);
+    sendForm(formBootom);
+    */
+
+    //form whis promise part 2
+    let message = {
+        loading: 'Загрузка...',
+        success: 'Спасибо! Скоро мы с вами свяжемся!',
+        failure: 'Что-то пошло не так...'
+    };
+    
+    let form = document.querySelector('.main-form'),
+        formBootom = document.getElementById('form'),
+        input = form.getElementsByTagName('input'),
+        statusMessage = document.createElement('div'); //создаем див для передачи пользователю сообщения статуса    
+        statusMessage.classList.add('status');
+    
+    function sendForm(elem){
+        elem.addEventListener('submit', function(event) {//вещаем обработчик события именно на форму, а не на кнопку в форме, потому что нам нужно знать когда именно форма отправляется на сервер
+            event.preventDefault(); //для того чтобы не обновлялась страница при нажатии на кнопку форму(обновление браузера)
+            elem.appendChild(statusMessage);
+    
+            let formData = new FormData(elem); //с помощью объекта формдата получаем все что пользовтаель ответил  в форме
+    
+            
+                function postData(data){
+                    return new Promise(function(resolve,reject){
+                        let request = new XMLHttpRequest();
+                        request.open('POST', 'server.php');
+                        //request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded'); 
+                        
+                        //for json
+                        request.setRequestHeader('Content-type', 'application/json; charset=uft-8');
+                        //end for json
+                        request.onreadystatechange = function(){ //чтобы отлеживать статусы отправлений
+                            if(request.readyState<4){
+                               resolve();
+                            }else if(request.readyState === 4){
+                                if(request.status == 200 && request.status < 3){
+                                    resolve();
+                                }
+                                else{
+                                    reject();
+                                }
+                            }
+                        }
+                        //for json
+                    let obj ={};  //создем новый объект в который помещаем данные
+    
+                    data.forEach(function(value,key) { //с помощью метода forEach берем все данные из формдаты и помещаем в объект obj
+                            obj[key] = value;
+                        });
+                    let json = JSON.stringify(obj); //превращаем в json формат
+                    request.send(json); // отправляем на сервер
+                    //end for json
+                    //request.send(formData); //форм дата - это данные которые ввел пользователь 
+                    })
+                }
+                
+                function clearInput() {
+                    for (let i=0; i<input.length; i++){
+                        input[i].value='';
+                    }  
+                }
+            
+             
+    
+            postData(formData)
+                .then(() =>  statusMessage.innerHTML = message.loading)
+                .then(() => statusMessage.innerHTML = message.success)
+                .catch(() => statusMessage.innerHTML = message.failure)
+                .then(clearInput)
+        }); 
+    }
+    
+    sendForm(form);
+    sendForm(formBootom);
+    
+
+    //slider
+
+    let slideIndex = 1,  //переменная для переключения слайдов
+        slides = document.querySelectorAll('.slider-item'),
+        prev = document.querySelector('.prev'),
+        next = document.querySelector('.next'),
+        dotsWrap = document.querySelector('.slider-dots'),
+        dots = document.querySelectorAll('.dot');
+    
+    showSlides(slideIndex);
+    
+    function showSlides(n){
+        if(n > slides.length){  //если номер слайда больше чем количесво слайдов
+            slideIndex=1; //если слайды закончились, то мы возвращаемся к самому первому
+        }
+        if(n < 1){ //если листаем слайд назад, то возвращамся к последнему
+            slideIndex = slides.length;
+        }
+
+        slides.forEach((item) => item.style.display = 'none'); //показываем слайд который нам нужен
+        /*for (let i = 0; i < slides.length; i++){
+            slides[i].style.display = 'none';
+        }*/  //тоже самое что строка выше
+        dots.forEach((item) => item.classList.remove('dot-active'));//у всех точек убираем класс activ
+
+        slides[slideIndex - 1].style.display = 'block';
+        dots[slideIndex - 1].classList.add('dot-active');
+        }
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
+
+    prev.addEventListener('click', function(){
+        plusSlides(-1);
+    });
+
+    next.addEventListener('click', function(){
+        plusSlides(1);
+    });
+
+    dotsWrap.addEventListener('click', function(event){
+        for (let i=0; i < dots.length +1; i++){
+            if(event.target.classList.contains('dot') && event.target == dots[i-1]){
+                currentSlide(i);
+            }
+        }
+    });
+
 });
